97b98cbf4aa2e13f06e5dfd6f06b583d019c7f56
SadeFrancis <122968872+SadeFrancis@users.noreply.github.com>
2024-04-09 13:20:29 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import altair as alt
 import numpy as np
 #load participant usernames
-participant_progress=pd.read_csv('/content/Partcipant Tracking Spreadsheet - Eligible Subs Progress Survey Comp.csv')
+participant_progress=pd.read_csv('Partcipant Tracking Spreadsheet - Eligible Subs Progress Survey Comp.csv')
 df_partic_prog= pd.DataFrame(participant_progress, columns=['Eligible Participant', 'Assigned Username'])
 usernames=df_partic_prog['Assigned Username']
 st.write(usernames)

eeb173a0b12d92779eb1f66effcfe94a64f1173c
SadeFrancis <122968872+SadeFrancis@users.noreply.github.com>
2024-04-09 14:11:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,5 +14,26 @@ st.title('Energy Consumption Leaderboard')
 energy_consdata=pd.read_csv('Partcipant Tracking Spreadsheet - Example Energy Data.csv')
 df_energy_data=pd.DataFrame(energy_consdata, columns=['username', 'new energy cons', 'old energy cons', 'energy change'])
 
+#Participants must enter username and email to login
+pip install streamlit-authenticator
+import streamlit_authenticator as stauth
+#create YAML file 
+credentials:
+preauthorized:
+usernames: 
+-usernames.tolist()
+#need to create a list of the preauthorizes usernames and corresponding emails
+import yaml 
+from yaml.loader impirt SafeLoader 
+with open ('../config.yaml') as file
+  config = yaml.load(file, Loader-SafeLoader)
+authenticator= Authenticate( 
+  config['credentials'],
+  config['preauthroized']
+)
+#authentication pop up
+name, authentucation_status, username= authenticator.login('Login', 'main')
+
+#read authentication and forgotten username feauture 
 
 

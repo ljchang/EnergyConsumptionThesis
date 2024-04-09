3c3ed67189fbddca59198d509f4a5c7614c39851
SadeFrancis <122968872+SadeFrancis@users.noreply.github.com>
2024-04-09 13:16:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,12 @@
+pip install streamlit
+#Import packages
+import streamlit as st
+import pandas as pd
+import altair as alt
+import numpy as np
+#load participant usernames
+participant_progress=pd.read_csv('/content/Partcipant Tracking Spreadsheet - Eligible Subs Progress Survey Comp.csv')
+df_partic_prog= pd.DataFrame(participant_progress, columns=['Eligible Participant', 'Assigned Username'])
+usernames=df_partic_prog['Assigned Username']
+
 

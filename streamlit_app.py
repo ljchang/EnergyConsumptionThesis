148292c6d4939696544a6f396260560ae62b09a5
SadeFrancis <122968872+SadeFrancis@users.noreply.github.com>
2024-04-09 13:44:22 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,12 @@ import numpy as np
 participant_progress=pd.read_csv('Partcipant Tracking Spreadsheet - Eligible Subs Progress Survey Comp.csv')
 df_partic_prog= pd.DataFrame(participant_progress, columns=['Eligible Participant', 'Assigned Username'])
 usernames=df_partic_prog['Assigned Username']
-st.write(usernames)
+#st.write(usernames)
+st.title('Energy Consumption Leaderboard')
+
+#load energy consumption data
+energy_consdata=pd.read_csv('Partcipant Tracking Spreadsheet - Example Energy Data.csv')
+df_energy_data=pd.DataFrame(energy_consdata, columns=['username', 'new energy cons', 'old energy cons', 'energy change')
+
 
 
